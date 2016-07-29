b5561946b3384f7ffe3a70ea2080194fb92d1eff
Alberto Gaona <albertoo_3c@hotmail.com>
2016-07-29 18:02:47 -0500

    finish crawl web

--- ICS/search_engine.py
+++ ICS/search_engine.py
@@ -1,3 +1,7 @@
+############################################################################################################################################
+"""Web crawler, for any seed page, it will find all the pages that can 
+be reached from that page, and return them in a list"""
+
 def get_page(url):
 	try:
 		import urllib.request
@@ -14,19 +18,37 @@ def get_next_target(page):
 	url = page[start_quote+1:end_quote]
 	return url, end_quote
 
-def print_all_links(page):
+def union(list_one,list_two):
+	for element in list_two:
+		if(element not in list_one):
+			list_one.append(element)
+
+def get_all_links(page):
+	links = []
 	while(True):
 		url, endpos = get_next_target(page)
 		if url:
-			print(url)
+			links.append(url)
 			page = page[endpos:]
 		else:
 			break
+	return links
+
+def crawl_web(seed):
+	tocrawl = [seed]
+	crawled = []
+	while tocrawl:
+		page = tocrawl.pop()
+		if(page not in crawled):
+			union(tocrawl,get_all_links(get_page(page)))
+			crawled.append(page)
+	return crawled
+
 
 
 #print(get_page("http://xkcd.com/353/")) # Test get_page
 #print(get_next_target('<li><a href="http://blag.xkcd.com">Blag</a></li>')) #Test get_next_target
-#print_all_links('<li><a href="http://blag.xkcd.com">Blag</a></li><li><a href="http://store.xkcd.com/">Store</a></li>') # Test print_all_links
- 
-
-print_all_links(get_page('http://xkcd.com/353/'))   
+#get_all_links('<li><a href="http://blag.xkcd.com">Blag</a></li><li><a href="http://store.xkcd.com/">Store</a></li>') # Test print_all_links
+#print(get_all_links(get_page('http://xkcd.com/353/')))
+print(crawl_web("https://www.udacity.com/cs101x/index.html?_ga=1.161011645.1417601336.1463164125"))
+############################################################################################################################################
