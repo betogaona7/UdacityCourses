6304f110623718d2524c9e16cf05198bf22315a4
Alberto Gaona <albertoo_3c@hotmail.com>
2016-08-01 22:48:11 -0500

    fibonacci

---
+++ ICS/fibonacci.py
@@ -0,0 +1,20 @@
+
+#Recursive
+def fibonacci_recursive(n):
+	if n == 0:
+		return 0
+	if n == 1:
+		return 1
+	return fibonacci_recursive(n-1)+fibonacci_recursive(n-2)
+
+#print (fibonacci_recursive(36))
+
+#Iterative 
+def fibonacci_iterative(n):
+	current = 0
+	after = 1
+	for i in range(0,n):
+		current, after = after, current + after
+	return current
+
+print (fibonacci_iterative(36))
