bcd75ec365e59fd8abdcb86da39b21ea999f030e
Alberto Gaona <albertoo_3c@hotmail.com>
2016-08-04 14:40:55 -0500

    final search engine

--- ICS/search_engine.py
+++ ICS/search_engine.py
@@ -45,6 +45,11 @@ def get_all_links(page):
 #			crawled.append(page)
 #	return crawled
 
+""" Three main issues with scaling up a web crawler
+- One is the normal politeness that you need on the web
+- How you get a bunch of machines involved in crawling not just one
+- How to consume a lot of bandwith so that you keep the expensive resources busy, while still being polite """
+
 ############################################################################################################################################
 """ Make a index """
 
@@ -119,14 +124,29 @@ def compute_ranks(graph):
             newrank = (1 - d) / npages
             for node in graph:
                 if page in graph[node]:
-				    newrank = newrank + d * (ranks[node] / len(graph[node]))
+                	newrank = newrank+d*(ranks[node]/len(graph[node]))
             newranks[page] = newrank
         ranks = newranks
     return ranks
 
+def lucky_search(index, ranks, keyword):
+	pages = lookup(index, keyword)
+	if not pages:
+		return None 
+	best_page = pages[0]
+	for candidate in pages:
+		if ranks[candidate] > ranks[best_page]:
+			best_page = candidate
+	return best_page
 
 
 #print all index
-print(crawl_web("https://www.udacity.com/cs101x/index.html?_ga=1.161011645.1417601336.1463164125"))
+#print(crawl_web("https://www.udacity.com/cs101x/index.html?_ga=1.161011645.1417601336.1463164125"))
 #print all pages with keyword "learning"
-print("\n" + str(lookup(crawl_web("https://www.udacity.com/cs101x/index.html?_ga=1.161011645.1417601336.1463164125"),"learning")))
+#print("\n" + str(lookup(crawl_web("https://www.udacity.com/cs101x/index.html?_ga=1.161011645.1417601336.1463164125"),"learning")))
+
+#Output: Highest Rating page with keyword 
+index, graph = crawl_web("https://www.udacity.com/cs101x/index.html?_ga=1.161011645.1417601336.1463164125")
+ranks = compute_ranks(graph)
+
+print (lucky_search(index, ranks, "learning"))
