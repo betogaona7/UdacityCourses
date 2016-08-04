13d4239771bf93afe584db9e6e4aac62e73ee3af
Alberto Gaona <albertoo_3c@hotmail.com>
2016-08-03 22:47:49 -0500

    add compute_rank

--- ICS/search_engine.py
+++ ICS/search_engine.py
@@ -108,3 +108,23 @@ def crawl_web(seed):
 			union(tocrawl,outlinks)
 			crawled.append(page)
 	return index, graph
+
+def compute_ranks(graph):
+    d = 0.8 # damping factor, probabilty
+    numloops = 10
+    
+    ranks = {}
+    npages = len(graph)
+    for page in graph:
+        ranks[page] = 1.0 / npages
+    
+    for i in range(0, numloops):
+        newranks = {}
+        for page in graph:
+            newrank = (1 - d) / npages
+            for node in graph:
+                if page in graph[node]:
+				    newrank = newrank + d * (ranks[node] / len(graph[node]))
+            newranks[page] = newrank
+        ranks = newranks
+    return ranks
